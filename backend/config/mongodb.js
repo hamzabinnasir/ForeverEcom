172754823487996ea0fd654f92a2e41e3fc8f7f3
Hamza Bin Nasir <141481692+hamzabinnasir@users.noreply.github.com>
2025-08-01 17:40:24 +0500

    Update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,18 +1,9 @@
-import mongoose from "mongoose";
-
+import mongoose from "mongoose"
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
     mongoose.connection.on("connected", () => {
-      console.log("✅ MongoDB connected successfully");
+        console.log("DB Connected");
     });
-
-    mongoose.connection.on("error", (err) => {
-      console.error("❌ MongoDB connection error:", err);
-    });
-};
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
+}
 
 export default connectDB;
