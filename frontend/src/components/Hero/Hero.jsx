ebfc2efbf857a81657a7a19a3a57d40723afadd3
Hamza Bin Nasir <141481692+hamzabinnasir@users.noreply.github.com>
2025-08-09 17:28:37 +0500

    Update Hero.jsx

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import "./hero.css"
 import React from "react"
 // import heroImg from "../../assets/hero_img.png"
-import heroImg2 from "../../assets/heroImg2.jpg"
+import heroImg5 from "../../assets/heroImg5.jpg"
 export default function Hero() {
     return (
         <>
@@ -20,13 +20,14 @@ export default function Hero() {
                     </div>
                 </div>
                 <div className="heroRight">
-                    <img src={heroImg2} alt="no img" />
+                    <img src={heroImg5} alt="no img" />
                 </div>
             </div>
         </>
     )
 
 }
+
 
 
 
